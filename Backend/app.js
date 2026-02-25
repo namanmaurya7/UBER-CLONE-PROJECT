ed3becdc162a8d363bd8d3e4d4a986426d9b7a28
Naman Maurya <mauryanaman367@gmail.com>
2026-02-25 23:26:54 +0530

    user authentication done

--- Backend/app.js
+++ Backend/app.js
@@ -2,14 +2,18 @@ const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
 const app = express();
+const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const connectToDb = require('./db/db.js');
 const userRoutes = require('./routes/user.routes.js');
+
+
 connectToDb();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 
 
